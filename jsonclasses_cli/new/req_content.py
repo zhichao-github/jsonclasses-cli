d8e76a3948ca65bb6c0fc551a12c1b2b031f987b
Victor Teo <victor.teo@fillmula.com>
2021-11-10 15:55:34 +0800

    update deps

--- jsonclasses_cli/new/req_content.py
+++ jsonclasses_cli/new/req_content.py
@@ -6,9 +6,9 @@ def req_content(http_library: Literal['flask', 'fastapi'],
                 include_admin: bool) -> str:
     nl = '\n'
     return f"""
-jsonclasses>=3.0.0,<4.0.0
-jsonclasses-pymongo>=3.0.0,<4.0.0
-jsonclasses-server>=3.0.0,<4.0.0
+jsonclasses>=3.1.3,<4.0.0
+jsonclasses-pymongo>=3.1.3,<4.0.0
+jsonclasses-server>=3.1.3,<4.0.0
 inflection>=0.5.1,<0.6.0
 {'flask[async]>=2.0.1,<2.1.0' if http_library == 'flask' else 'fastapi[all]>=0.70.0,<0.71.0'}
 python-dotenv>=0.19.1,<0.20.0{f'{nl}bcrypt>=3.2.0,<4.0.0' if include_admin or include_user else ''}
