65cefa598ebf9e33bd7ad169bb41d02cd3667a69
Ben Wong <ben.wong@fillmula.com>
2021-11-23 19:31:49 +0800

    update unary sort order

--- jsonclasses_cli/package/swift/unary_sort_order.py
+++ jsonclasses_cli/package/swift/unary_sort_order.py
@@ -1,16 +1,9 @@
-from typing import Callable
 from ...utils.join_lines import join_lines
 
-
-def unary_sort_order(
-        name: str,
-        items: list[str],
-        mapper: Callable[[str], str]) -> str:
+def unary_sort_order(name: str):
     return join_lines([
         _unary_sort_order_first_line(name),
-        _unary_sort_order_switch_line(),
-        *map(lambda item: _case_group(item, mapper), items),
-        _unary_sort_order_switch_back_line(),
+        _unary_sort_if_block(name),
         _unary_sort_order_last_line()
     ])
 
@@ -18,23 +11,14 @@ def unary_sort_order(
 def _unary_sort_order_first_line(name: str) -> str:
     return f'public prefix func -(rhs: {name}) -> {name} {"{"}'
 
-
-def _unary_sort_order_switch_line() -> str:
-    return '    switch rhs {'
-
-
-def _unary_sort_order_switch_back_line() -> str:
-    return '    }'
-
+def _unary_sort_if_block(name: str) -> str:
+    return join_lines([
+        f'    if rhs.rawValue.contains("-") {"{"}',
+        f'        return {name}(rawValue: String(rhs.rawValue.dropFirst()))!',
+        f'    {"}"} else {"{"}',
+        f'        return {name}(rawValue: "-" + rhs.rawValue)!',
+        f'    {"}"}'
+    ])
 
 def _unary_sort_order_last_line() -> str:
     return '}'
-
-
-def _case_group(item: str, desc: Callable[[str], str]) -> str:
-    return join_lines([
-        f'    case .{item}:',
-        f'        return .{desc(item)}',
-        f'    case .{desc(item)}:',
-        f'        return .{item}'
-    ])
