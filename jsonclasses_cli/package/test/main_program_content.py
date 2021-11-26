3c6cbd0d82aefe0c84dd3839c0a69b089ebe9c44
Ben Wong <ben.wong@fillmula.com>
2021-11-26 17:47:13 +0800

    add session and session input

--- jsonclasses_cli/package/test/main_program_content.py
+++ jsonclasses_cli/package/test/main_program_content.py
@@ -1,4 +1,9 @@
 from jsonclasses.cgraph import CGraph
+
+from jsonclasses_cli.package.test.session_input import session_input
+
+from .session import session
+from .session_items import session_items
 from .data_interface import data_interface
 from .string_query import string_query
 from .number_query import number_query
@@ -9,6 +14,8 @@ from ...utils.join_lines import join_lines
 
 
 def main_program_content(cgraph: CGraph) -> str:
+    session_classes = session_items(cgraph)
+    use_session = len(session_classes) > 0
     return join_lines([
         _import_lines(),
         *map(lambda e: data_enum(e), cgraph._enum_map.values()),
@@ -17,6 +24,8 @@ def main_program_content(cgraph: CGraph) -> str:
         boolean_query(),
         date_query(),
         *map(lambda c: data_interface(c), cgraph._map.values()),
+        *map(lambda c: session_input(c), cgraph._map.values()),
+        session(session_classes)
     ], 3)
 
 
