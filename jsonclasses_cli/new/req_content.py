26a6da913e512c54d03c32d779e88a7d6a523363
Ben Wong <ben.wong@fillmula.com>
2021-12-31 15:32:45 +0800

    update requirements.txt  version

--- jsonclasses_cli/new/req_content.py
+++ jsonclasses_cli/new/req_content.py
@@ -1,9 +1,9 @@
 def req_content(include_user: bool, include_admin: bool) -> str:
     nl = '\n'
     return f"""
-jsonclasses>=3.1.4,<4.0.0
-jsonclasses-pymongo>=3.1.4,<4.0.0
-jsonclasses-server>=3.1.4,<4.0.0
+jsonclasses>=3.2.0,<4.0.0
+jsonclasses-pymongo>=3.2.0,<4.0.0
+jsonclasses-server>=3.2.0,<4.0.0
 thunderlight>=0.1.2,<1.0.0
 inflection-plus>=0.1.0,<1.0.0{f'{nl}bcrypt>=3.2.0,<4.0.0' if include_admin or include_user else ''}
     """.strip() + '\n'
