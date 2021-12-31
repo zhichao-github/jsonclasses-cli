ad37a22e2e9867371291413ecd24892366b3c1bc
Ben Wong <ben.wong@fillmula.com>
2021-12-31 15:23:07 +0800

    rewrite config.json

--- jsonclasses_cli/new/conf_content.py
+++ jsonclasses_cli/new/conf_content.py
@@ -1,12 +1,13 @@
 from random import sample
-
 from jsonclasses_cli.utils.join_lines import join_lines
 
 
-def conf_content() -> str:
+def conf_content(name: str) -> str:
     return join_lines([
         '{',
-        _operator_conf,
+        _pymongo_conf(name),
+        _operator_conf(),
+        _package_conf(),
         '}'
     ])
 
@@ -15,14 +16,34 @@ def _operator_conf() -> str:
     return f"""
     "operator": {'{'}
         "secretKey": "{_randompuncs()}"
-    {'}'}
-    """.strip() + '/n'
+    {'}'},""".strip('\n')
 
 
 def _randompuncs() -> str:
     ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
     ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    punctuation = r"""!#$%&()*+,-./:;<=>?@[\]^_`{|}~"""
+    punctuation = r"""!#$%&()*+,-./:;<=>?@[/]^_`{|}~"""
     digits = '0123456789'
     ascii_letters = ascii_lowercase + ascii_uppercase
     return ''.join(sample(punctuation+ascii_letters+digits, 24))
+
+
+def _pymongo_conf(dbname: str) -> str:
+    return f"""
+    "pymongo": {'{'}
+        "url": "mongodb://localhost:27017/{dbname}"
+    {'}'},""".strip('\n')
+
+def _package_conf() -> str:
+    return f"""
+    "package": {'{'}
+        "ts": {'{'}
+            "url": "http://127.0.0.1:8000"
+        {'}'},
+        "swift": {'{'}
+            "url": "http://127.0.0.1:8000"
+        {'}'},
+        "kotlin": {'{'}
+            "url": "http://127.0.0.1:8000"
+        {'}'}
+    {'}'}""".strip('\n')
