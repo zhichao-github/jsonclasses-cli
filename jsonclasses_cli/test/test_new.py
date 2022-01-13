b1fa0f5a956bd8540efd25fdb7714693838b5c36
Ben Wong <ben.wong@fillmula.com>
2022-01-13 23:07:34 +0800

    init test case

--- jsonclasses_cli/test/test_new.py
+++ jsonclasses_cli/test/test_new.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+from unittest.mock import patch
 from unittest import TestCase
 from tempfile import TemporaryDirectory
 from pathlib import Path
-from ..new import new
+from ..new import new, app_content
 
 
 class TestNew(TestCase):
@@ -14,7 +15,7 @@ class TestNew(TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
-        pass
+        cls.temp_dir.cleanup()
 
     def setUp(self) -> None:
         self.__class__.temp_dir.cleanup()
@@ -23,4 +24,19 @@ class TestNew(TestCase):
     def test_new_create_all_filed(self) -> None:
         new(self.temp_path, None, None, None, None, None, True)
         app = Path(self.temp_path) / 'app.py'
-        print(app.read_text())
+        requirements = Path(self.temp_path) / 'requirements.txt'
+        config = Path(self.temp_path) / 'config.json'
+        mypy = Path(self.temp_path) / 'mypy.ini'
+        gitignore = Path(self.temp_path) / '.gitignore'
+        read_me = Path(self.temp_path) / 'README.md'
+        self.assertTrue(app.is_file(), app.name)
+        self.assertTrue(requirements.is_file(), requirements.name)
+        self.assertTrue(config.is_file(), config.name)
+        self.assertTrue(mypy.is_file(), mypy.name)
+        self.assertTrue(gitignore.is_file(), gitignore.name)
+        self.assertTrue(read_me.is_file(), read_me.name)
+
+    def test_new_create_app_filed_without_user_and_admin(self) -> None:
+        new(self.temp_path, None, False, False, None, None, True)
+        app = Path(self.temp_path) / 'app.py'
+        self.assertEqual(app.read_text(), app_content(False, False))
