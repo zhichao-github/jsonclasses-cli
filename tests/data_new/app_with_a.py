5b17a58157ed21ef934b053e11a719fe7e02da40
Ben Wong <ben.wong@fillmula.com>
2022-01-18 13:11:41 +0800

    test new func

---
+++ tests/data_new/app_with_a.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+from datetime import datetime
+from jsonclasses import jsonclass, types
+from jsonclasses_pymongo import pymongo
+from jsonclasses_server import api, authorized, server
+
+
+
+
+# You can create a model by using class and type hint syntax, with decorators
+# decorated.
+#
+# Models with relationships are supported.
+#
+# To understand the types modifier pipeline syntax, check our documentation:
+#
+# https://docs.jsonclasses.com/docs/api-documentation/types-modifiers
+#
+# @api
+# @pymongo
+# @jsonclass
+# class MyModel:
+#     id: str = types.readonly.str.primary.mongoid.required
+#     one: str
+#     two: int = 1
+#     created_at: datetime = types.readonly.datetime.tscreated.required
+#     updated_at: datetime = types.readonly.datetime.tsupdated.required
+
+
+@authorized
+@api
+@pymongo
+@jsonclass
+class Admin:
+    id: str = types.readonly.str.primary.mongoid.required
+    email: str = types.str.email.unique.authidentity.required
+    password: str = types.writeonly.str.securepw.length(8, 16).salt.authbycheckpw.required
+    name: str | None
+    created_at: datetime = types.readonly.datetime.tscreated.required
+    updated_at: datetime = types.readonly.datetime.tsupdated.required
+
+
+app = server()
+
+
+# You can still write custom routes if synthesized routes don't satisfy your
+# need. To understand the ORM methods, check our documentation:
+#
+# https://docs.jsonclasses.com/docs/api-documentation/orm-addons
--- tests/test_package.py
+++ tests/test_package.py
@@ -22,5 +22,5 @@ class TestPackage(TestCase):
     def test_create_simple_song_ts_package(self) -> None:
         simple_song_path = self.cls_dir / 'simple_song.py'
         package(self.temp_path, simple_song_path, 'ts', True)
-        print(simple_song_path)
+        # print(simple_song_path)
         # self.assertEqual()
