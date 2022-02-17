f7ea36395b2983032060cb1c5cd7ee42f8134d4c
Ben Wong <ben.wong@fillmula.com>
2022-02-17 13:48:32 +0800

    fix install set up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """setup.py"""
 from pathlib import Path
 from setuptools import setup, find_packages
-from jsonclasses_cli.version import version
+exec(open("jsonclasses_cli/version.py").read())
 # The text of the README file
 README = (Path(__file__).parent / "README.md").read_text()
 
